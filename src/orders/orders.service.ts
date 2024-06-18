2cbbe92cd1f2380442b3f60e91b91d6177545270
RodrigoDuvivier <rodrigo.martinez224@gmail.com>
2024-06-18 08:37:04 -0300

    Feature-database

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,10 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
+import { PrismaClient } from '@prisma/client';
 
 
 @Injectable()
-export class OrdersService {
+export class OrdersService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger('OrdersService')
+
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('database connected')
+  }
   create(createOrderDto: CreateOrderDto) {
     return 'This action adds a new order';
   }
